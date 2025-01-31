5cf73d1ccbf2ad56396e7cd14701b4016a2be7e4
Sherry Yang <19577448+sherzyang@users.noreply.github.com>
2025-01-30 19:11:31 -0800

    Update.

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+let totalBatteries = batteryBatches.reduce(superCounter, 0)
+
+function superCounter(accumulator, element){
+    return element + accumulator;
+}
